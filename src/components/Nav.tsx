8a467d46154e4e396fd1a3f682f2badd52c1a0c3
“rhunter27” <ravenhunter27@gmail.com>
2025-05-25 13:27:05 -0400

    added and edited files

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
+
+// Removed duplicate function Nav
+
 const Nav = () => {
   return (
     <nav>
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
 
